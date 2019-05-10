6f1e212f94ffb5a618bb016e66da8d3ed42bb9fa
Tomoyuki Mano <tomoyukimano@gmail.com>
2019-05-10 19:00:58 +0900

    please test this

--- leapmonth.py
+++ leapmonth.py
@@ -3,7 +3,24 @@ import sys
 def main():
     year = int(sys.argv[1])
     month = int(sys.argv[2])
-    print("Month of ", month, "/", year, " has ", 31, " days.", sep="")
+
+    # convert to integer
+    year = int( year )
+    month = int( month )
+
+    # if section
+    if month in [ 1, 3, 5, 7, 8, 10, 12 ]:
+    	num_days = 31
+    elif month in [ 4, 6, 9, 11 ]:
+    	num_days = 30
+    else:
+    	# if month = februrary
+    	if year%4 == 0:
+    		num_days = 29
+    	else:
+    		num_days = 28
+    		
+    print("Month of ", month, "/", year, " has ", num_days, " days.")
 
 if __name__ == '__main__':
     main()
